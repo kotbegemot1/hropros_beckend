3dc257db87ed8308802d6b853b9e54fdfe586052
kk1slorod <kk1slorod@gmail.com>
2020-12-08 20:18:13 +0300

    commit for heroku

--- hropros/settings.py
+++ hropros/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', "vast-earth-53225.herokuapp.com"]
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
